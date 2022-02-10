6f41c7eb47d54a0d005d78f03e860303da6a3d53
Peter Gallagher <pete@pjgcreations.co.uk>
2022-02-10 20:50:56 +0000

    Added Event Logging to test VM script

--- vm_scripts/install_vmx.ps1
+++ vm_scripts/install_vmx.ps1
@@ -1,3 +1,7 @@
+Write-EventLog -LogName "Application" -Source "InstallVM1" -EventID 3001 -EntryType Information -Message (-join("Connection String = ", $args[0])) -Category 1 -RawData 10,20
+Write-EventLog -LogName "Application" -Source "InstallVM1" -EventID 3001 -EntryType Information -Message (-join("Account Key = ", $args[1])) -Category 1 -RawData 10,20
+Write-EventLog -LogName "Application" -Source "InstallVM1" -EventID 3001 -EntryType Information -Message (-join("License Key = ", $args[2])) -Category 1 -RawData 10,20
+
 (-join("Connection String = ", $args[0])) | Out-File -FilePath "C:\args.txt"
 (-join("Account Key = ", $args[1])) | Out-File -FilePath "C:\args.txt"
 (-join("License Key = ", $args[2])) | Out-File -FilePath "C:\args.txt"
