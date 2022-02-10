0d26ecc7d10b684e7d9cf3c894507f819326f6d9
Peter Gallagher <pete@pjgcreations.co.uk>
2022-02-10 17:12:52 +0000

    Fix Load Balancer Ip Allocation

--- provision_azure_scripts/01_create_infra.ps1
+++ provision_azure_scripts/01_create_infra.ps1
@@ -72,18 +72,25 @@ az deployment group create --resource-group $resourceGroup `
 
 # Create Load Balancer BackEnd Pools
 
+Write-Output "Creating Load Balancer Backend Pools"
+
 $loadBalancerName = (-join($prefix, "-lb"))
 $vnetName = (-join($prefix, "-vnet"))
 $networkInterface_1_ipAddress = '172.27.0.4'
 $networkInterface_2_ipAddress = '172.27.0.5'
+
+Write-Output (-join("Load Balancer Name = ", $loadBalancerName))
+Write-Output (-join("VNet Name = ", $vnetName))
+Write-Output (-join("IP 1 Address = ", $networkInterface_1_ipAddress))
+Write-Output (-join("IP 2 Address = ", $networkInterface_2_ipAddress))
 
 $loadBalancer = Get-AzLoadBalancer -ResourceGroupName $resourceGroup -Name $loadBalancerName
 $backendPool = $lb | Get-AzLoadBalancerBackendAddressPool
 
 $virtualNetwork = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroup 
  
-$ip1 = New-AzLoadBalancerBackendAddressConfig -IpAddress $networkInterface_1_ipAddress -Name "TestVNetRef1" -VirtualNetwork $virtualNetwork.id
-$ip2 = New-AzLoadBalancerBackendAddressConfig -IpAddress $networkInterface_2_ipAddress -Name "TestVNetRef2" -VirtualNetwork $virtualNetwork.id
+$ip1 = New-AzLoadBalancerBackendAddressConfig -IpAddress $networkInterface_1_ipAddress -Name "VNetRef1" -VirtualNetwork $virtualNetwork.id
+$ip2 = New-AzLoadBalancerBackendAddressConfig -IpAddress $networkInterface_2_ipAddress -Name "VNetRef2" -VirtualNetwork $virtualNetwork.id
  
 $backendPool.LoadBalancerBackendAddresses.Add($ip1) 
 $backendPool.LoadBalancerBackendAddresses.Add($ip2)
